c984579dbe29e6a7b8796b4adedd069eeb8be481
The Rebel Idea One <therebelideaone@Thes-iMac.local>
2019-05-14 14:28:38 -0400

    We change so many thing here. It's amazing

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?
 /*
 Name: Design Depot 2020
+Version: 1.01
 */
 
 echo "this is the best day of my life";
-echo "<h1>This is crazy<h1>";
+echo "<h1>This is crazy</h1>";
+echo "<h2> Check the new branch";
